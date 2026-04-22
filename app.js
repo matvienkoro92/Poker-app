31c7de9c5721d99a0f0e8463cdf916e19c3d6a4e
Космонавт <kosmonavt@MacBook-Air-Kosmonavt.local>
2026-04-22 16:59:20 +0700

    Hide email link form when email is already linked and bump version to 2.024

--- app.js
+++ app.js
@@ -14833,7 +14833,7 @@ function syncProfileEmailAuthUi() {
     else if (linkedEmail) textEl.textContent = "Эта почта уже привязана. По ней можно входить в аккаунт на экране авторизации.";
     else textEl.textContent = "Привяжите email, чтобы потом можно было входить в аккаунт по почте.";
   }
-  if (formWrap) formWrap.hidden = !!(authMethod === "email" && linkedEmail);
+  if (formWrap) formWrap.hidden = !!linkedEmail;
   var disableInputs = !isVerified || isGuest;
   if (emailInput) {
     emailInput.disabled = disableInputs;
