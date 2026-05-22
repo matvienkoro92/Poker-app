86ee096e5f845a364d4b30a99b532cd0772c4747
Космонавт <kosmonavt@MacBook-Air-Kosmonavt.local>
2026-05-23 01:27:56 +0700

    Fix daily poker Android scrolling

--- app-view-router.js
+++ app-view-router.js
@@ -1198,6 +1198,7 @@ function setView(viewName, navOpts) {
   document.documentElement.classList.toggle("app-view-profile-html-scroll", viewName === "profile");
   document.documentElement.classList.toggle("app-view-video-lessons-html-scroll", viewName === "video-lessons");
   document.documentElement.classList.toggle("app-view-raffles-html-scroll", viewName === "raffles");
+  document.documentElement.classList.toggle("app-view-daily-poker-html-scroll", viewName === "daily-poker");
   document.documentElement.classList.toggle("app-view-player-crm-html-scroll", viewName === "player-crm");
   var appEl = document.getElementById("app");
   if (appEl) appEl.classList.toggle("app--view-home", viewName === "home");
