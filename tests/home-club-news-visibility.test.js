7f1daaaadcc407dfafb795ccaebbbd8baa1193bc
matvienkoro92 <matvienkoro92@gmail.com>
2026-09-04 00:45:26 +0700

    Fix rotating club news slides

--- tests/home-club-news-visibility.test.js
+++ tests/home-club-news-visibility.test.js
@@ -12,3 +12,11 @@ test("club news viewport does not use the iOS-breaking multilayer mask", functio
   assert.match(rule[1], /-webkit-mask-image:\s*none/);
   assert.match(rule[1], /mask-image:\s*none/);
 });
+
+test("club news track does not clip slides before they rotate into view", function () {
+  const css = fs.readFileSync(path.join(__dirname, "..", "styles.css"), "utf8");
+  const rule = css.match(/\.home-club-news__ticker \.home-friend-news__track\s*\{([^}]+)\}/);
+  assert.ok(rule);
+  assert.match(rule[1], /display:\s*flex/);
+  assert.match(rule[1], /overflow:\s*visible/);
+});
