60cea4038a4e592aae9006a08ceed7b8fb393569
matvienkoro92 <matvienkoro92@gmail.com>
2026-09-04 00:43:09 +0700

    Fix club news visibility on iOS

---
+++ tests/home-club-news-visibility.test.js
@@ -0,0 +1,14 @@
+"use strict";
+
+const test = require("node:test");
+const assert = require("node:assert/strict");
+const fs = require("node:fs");
+const path = require("node:path");
+
+test("club news viewport does not use the iOS-breaking multilayer mask", function () {
+  const css = fs.readFileSync(path.join(__dirname, "..", "styles-home-overrides.css"), "utf8");
+  const rule = css.match(/\.app\.app--view-home \.home-club-news__ticker \.home-friend-news__viewport\s*\{([^}]+)\}/);
+  assert.ok(rule);
+  assert.match(rule[1], /-webkit-mask-image:\s*none/);
+  assert.match(rule[1], /mask-image:\s*none/);
+});
